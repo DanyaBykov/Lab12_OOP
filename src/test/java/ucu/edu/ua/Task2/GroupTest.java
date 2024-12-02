19268cd1820fd7e6c73910966db8c83d8f9ae786
Danya Bykov <bykov.danya.ua@gmail.com>
2024-12-02 12:15:26 +0200

    fixed issue with tests

--- src/test/java/ucu/edu/ua/Task2/GroupTest.java
+++ src/test/java/ucu/edu/ua/Task2/GroupTest.java
@@ -1,7 +1,6 @@
 package ucu.edu.ua.Task2;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GroupTest {
@@ -11,18 +10,15 @@ public class GroupTest {
     private StringBuilder output1;
     private StringBuilder output2;
 
-    @BeforeEach
-    public void setUp() {
+    @Test
+    public void testApply() {
         output1 = new StringBuilder();
         output2 = new StringBuilder();
         task1 = new Signature<>(output1::append);
         task2 = new Signature<>(output2::append);
         group = new Group<>();
         group.addTask(task1).addTask(task2);
-    }
 
-    @Test
-    public void testApply() {
         group.apply("Test Argument");
         assertNotNull(group.getHeader("groupUuid"), "Group UUID should be set");
         assertEquals(group.getHeader("groupUuid"), task1.getHeader("groupUuid"), "Task1 should have the same group UUID");
@@ -33,6 +29,13 @@ public class GroupTest {
 
     @Test
     public void testFreeze() {
+        output1 = new StringBuilder();
+        output2 = new StringBuilder();
+        task1 = new Signature<>(output1::append);
+        task2 = new Signature<>(output2::append);
+        group = new Group<>();
+        group.addTask(task1).addTask(task2);
+
         group.freeze();
         assertNotNull(group.getHeader("groupUuid"), "Group UUID should be set after freeze");
         assertEquals(group.getHeader("groupUuid"), task1.getHeader("groupUuid"), "Task1 should have the same group UUID after freeze");
@@ -41,6 +44,13 @@ public class GroupTest {
 
     @Test
     public void testAddTask() {
+        output1 = new StringBuilder();
+        output2 = new StringBuilder();
+        task1 = new Signature<>(output1::append);
+        task2 = new Signature<>(output2::append);
+        group = new Group<>();
+        group.addTask(task1).addTask(task2);
+
         Signature<String> task3 = new Signature<>(output1::append);
         group.addTask(task3);
         assertEquals(3, group.getTaskCount(), "Group should contain 3 tasks");
